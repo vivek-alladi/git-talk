5a0d1814abd4b1559b6d74d41645642b1e4f5a48
Vivek Alladi <38089262+vivek-alladi@users.noreply.github.com>
2022-07-11 23:00:39 +0530

    for checking fetch

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
     public int add(int firstNumber, int secondNumber){
-        return firstNumber + secondNumber + firstNumber + secondNumber *2 +1+3;
+        int a=1;
+        return firstNumber + secondNumber + firstNumber + secondNumber *2 +1+3 +a;
     }
 }
