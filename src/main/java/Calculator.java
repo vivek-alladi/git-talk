14d7009adb763a9a65745ce50b4fef954f6bfe2a
Vivek Alladi <38089262+vivek-alladi@users.noreply.github.com>
2022-07-11 20:24:20 +0530

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
     public int add(int firstNumber, int secondNumber){
-        return firstNumber + secondNumber;
+        return firstNumber + secondNumber + firstNumber;
     }
 }
