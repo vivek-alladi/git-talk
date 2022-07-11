4c2bc672cc37eab4e2101363138dc3741fa0287f
Vivek Alladi <vivek.alladi@thoughtworks.com>
2022-07-11 23:05:52 +0530

    from local to remo

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
     public int add(int firstNumber, int secondNumber){
-        int a=1;
+        int a=1+2;
         return firstNumber + secondNumber + firstNumber + secondNumber *2 +1+3 +a;
     }
 }
